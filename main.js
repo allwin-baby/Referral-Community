a687556524a2739bd208e1aa612c21134e8539d2
ALLWIN BABY <allwin123321@gmail.com>
2024-11-26 20:22:52 +0530

    simple

--- main.js
+++
@@ -1,7 +0,0 @@
-        // Switch between the pages after 10 seconds
-        setTimeout(function() {
-            document.getElementById('page1').style.display = 'none';
-            document.getElementById('page2').style.display = 'flex';
-            document.body.style.backgroundColor = '#000'; // Black background
-            document.documentElement.style.backgroundColor = '#000'; // Black background
-        }, 10000); // 10000 milliseconds = 10 seconds
